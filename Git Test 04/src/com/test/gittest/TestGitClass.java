88f114641d8b15c21b28b5bfc9413eb9bf92fe60
mukhtiarmemon <mukhtiar001@yahoo.com>
2016-08-26 11:22:21 +0500

    junit project test 01

---
+++ Git Test 04/src/com/test/gittest/TestGitClass.java
@@ -0,0 +1,11 @@
+package com.test.gittest;
+
+public class TestGitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("test git 04 project");
+
+	}
+
+}
